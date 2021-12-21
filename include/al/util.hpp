3607b6bd72fbc41c97476f88c114604709c98552
Fruityloops <fruityloops@littun.co>
2021-12-21 21:17:53 +0100

    add setGravity 1.3

--- include/al/util.hpp
+++ include/al/util.hpp
@@ -184,14 +184,14 @@ namespace al
 
     #if(SMOVER==100)
     void setTrans(al::LiveActor *, sead::Vector3f const &);
+    void setGravity(al::LiveActor const *, sead::Vector3f const &);
     #endif
     #if(SMOVER==130)
     EFUN(0x0066D6D0, void, setTrans, EFUN_ARGS(const al::LiveActor* actor, const sead::Vector3f& trans));
+    EFUN(0x0066E050, void, setGravity, EFUN_ARGS(const al::LiveActor* actor, const sead::Vector3f& gravity));
     #endif
 
     void setScaleAll(al::LiveActor *, float);
-
-    void setGravity(al::LiveActor const *, sead::Vector3f const &);
 
     void setFront(al::LiveActor *, sead::Vector3f const &);
 
