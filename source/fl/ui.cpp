e53d2074aa11b10b1b8810383798829284552cec
Fruityloops <fruityloops@littun.co>
2021-12-27 19:45:41 +0100

    oop

--- source/fl/ui.cpp
+++ source/fl/ui.cpp
@@ -406,7 +406,7 @@ void fl::PracticeUI::menu()
 
                 #if SMOVER==100
                 TRIGGER("Connect to server\n", 1, {
-                    smo::Server::instance().connect("192.168.188.109", 7901);
+                    //smo::Server::instance().connect("someip", 7901);
                 });
                 TOGGLE("Old Motion Mod: %s\n", fl::TasHolder::instance().oldMotion, 2);
 
