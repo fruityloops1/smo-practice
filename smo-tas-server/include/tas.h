64d33f242b24f32011505b44bc9c240d518b901a
MonsterDruide1 <5958456@gmail.com>
2022-01-02 22:20:15 +0100

    `TasFrame`: explicit `0`-padding

--- smo-tas-server/include/tas.h
+++ smo-tas-server/include/tas.h
@@ -13,6 +13,8 @@ namespace fl
         bool L:1 = false, R:1 = false, ZL:1 = false, ZR:1 = false;
         bool plus:1 = false, minus:1 = false, pressLeftStick:1 = false, pressRightStick:1 = false;
         bool dUp:1 = false, dRight:1 = false, dDown:1 = false, dLeft:1 = false;
+
+        unsigned char padding[2]{0};
     };
 
     struct TasScript
