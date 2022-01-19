2bb4deeec02d4b4f45cafe141ae3a6060749deeb
Fruityloops <fruityloops@littun.co>
2022-01-19 19:33:59 +0100

    fix moon collect crash on 1.3

--- include/game/GameData/GameDataFile.h
+++ include/game/GameData/GameDataFile.h
@@ -18,14 +18,15 @@ class GameDataFile
         void wearCostume(char const *);
         void wearCap(char const *);
         void changeNextStage(ChangeStageInfo const*, int);
-        void setGotShine(const ShineInfo*);
         #if(SMOVER==100)
         PlayerHitPointData* getPlayerHitPointData() const;
         bool isGotShine(const ShineInfo*) const;
+        void setGotShine(const ShineInfo*);
         #endif
         #if(SMOVER==130)
         CVEFUN(GameDataFile, 0x004C8530, PlayerHitPointData*, getPlayerHitPointData);
         CEFUN(GameDataFile, 0x004C9880, bool, isGotShine, EFUN_ARGS(const ShineInfo* shineInfo), EFUN_ARGS(shineInfo));
+        VCEFUN(GameDataFile, 0x004C9880, setGotShine, EFUN_ARGS(const ShineInfo* info), EFUN_ARGS(info));
         #endif
 
         #if SMOVER==100
