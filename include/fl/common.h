eed94ac2a42dc9ea6502293fe29e3a01a5c6841c
Fruityloops <fruityloops@littun.co>
2022-04-13 02:42:55 +0200

    1.2.3

--- include/fl/common.h
+++ include/fl/common.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define PRACTICE_VERSTR "1.2.2"
-#define PRACTICE_VER 122
+#define PRACTICE_VERSTR "1.2.3"
+#define PRACTICE_VER 123
 
 #define MSG_NO130 "Not supported on 1.3 (currently)\n"
