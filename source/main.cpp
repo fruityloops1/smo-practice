ece4ff6af5cba00e229cd8902a5b7d94092f0451
Ontwikseltsaar <20740429+Ontwikseltsaar@users.noreply.github.com>
2022-05-30 17:21:38 +0200

    Update isDefeatKoopaLv1Var code for 1.3.0 using hacky logic.

--- source/main.cpp
+++ source/main.cpp
@@ -53,7 +53,29 @@ bool isEnableSaveVar(StageScene* stageScene)
 
 bool isDefeatKoopaLv1Var(StageScene* stageScene)
 {
+#if SMOVER == 100
     return fl::ui::PracticeUI::instance().skipBowser ? true : stageScene->isDefeatKoopaLv1();
+#endif
+#if SMOVER == 130
+    static int functionCalls = 0;
+    if (!fl::ui::PracticeUI::instance().skipBowser) {
+        functionCalls = 0;
+        return stageScene->isDefeatKoopaLv1();
+    }
+
+    // When debugging, returning true right away will skip Lost and go straight to (Day!) Metro with a broken Odyssey back in Wooded.
+    // Since isDefeatKoopaLv1 is called multiple times, we can return false the first time, then return true the second time, which
+    // seems to correctly skip the Bowser fight. This is a very hacky way of skipping the Bowser fight, so if there is some way to
+    // deduce from other game variables which function call we're in, that would be ideal. It would eliminate the need to introduce
+    // our own variable to keep track of the number of isDefeatKoopaLv1 calls.
+    functionCalls++;
+    if (functionCalls == 2) {
+        functionCalls = 0;
+        return true;
+    }
+
+    return false;
+#endif
 }
 
 bool isTriggerSnapShotModeVar(const al::IUseSceneObjHolder* objHolder)
