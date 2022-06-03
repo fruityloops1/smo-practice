ba64d3f35d0692a5f4314bbb39c25dbfe8fdda0e
Ontwikseltsaar <20740429+Ontwikseltsaar@users.noreply.github.com>
2022-06-03 16:02:25 +0200

    Update about page.

--- source/fl/ui/p_about.cpp
+++ source/fl/ui/p_about.cpp
@@ -3,8 +3,8 @@
 void fl::ui::about::update(PracticeUI& ui)
 {
     ui.printf("Welcome to the Practice Mod!\n");
-    ui.printf("Originally made by Fruityloops#8500\n");
-    ui.printf("TAS-Tools made by Mars2030#8008\n");
+    ui.printf("Originally created by Fruityloops#8500\n");
+    ui.printf("TAS-Tools implemented by Mars2030#8008\n");
     ui.printf("1.3-improvements by Ontwikseltsaar\n");
     ui.printf("Further updates by MonsterDruide1#7702\n");
     ui.printf("Big thanks to CraftyBoss and bryce_____!\n");
