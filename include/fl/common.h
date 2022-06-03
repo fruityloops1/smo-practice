2f7fd98155ffd7ee5345d2453cf9b4d51c35f1c4
Ontwikseltsaar <20740429+Ontwikseltsaar@users.noreply.github.com>
2022-06-03 16:00:34 +0200

    Update practice mod version number.

--- include/fl/common.h
+++ include/fl/common.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define PRACTICE_VERSTR "1.2.3"
-#define PRACTICE_VER 123
+#define PRACTICE_VERSTR "1.3.0"
+#define PRACTICE_VER 130
 
 #define MSG_NO130 "Not supported on 1.3 (currently)\n"
