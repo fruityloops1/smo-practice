31d73f87f34f8628d5aaa84fdbe6791651d0f7ef
Ontwikseltsaar <20740429+Ontwikseltsaar@users.noreply.github.com>
2022-06-05 23:34:07 +0200

    Enable the moon info page.

--- source/fl/ui/p_mooninfo.cpp
+++ source/fl/ui/p_mooninfo.cpp
@@ -10,12 +10,17 @@ void fl::ui::mooninfo::update(PracticeUI& ui)
     }
     ui.toggle("Hide Original Moon Counter", ui.hideShineCounter);
 
-#if (SMOVER == 100)
-    s32 currShine = GameDataFunction::getCurrentShineNum(*stageScene->mHolder);
-    s32 totalShine = GameDataFunction::getTotalShineNum(*stageScene->mHolder, -1);
-    s32 unlockShine = GameDataFunction::findUnlockShineNumByWorldId(0, *stageScene->mHolder, GameDataFunction::getCurrentWorldId(*stageScene->mHolder));
-    ui.printf("Current Moons: %d\n", currShine);
-    ui.printf("Total Moons: %d\n", totalShine);
-    ui.printf("Required Moons: %d", unlockShine);
+#if SMOVER == 100
+	s32 currShine = GameDataFunction::getCurrentShineNum(*stageScene->mHolder);
+	s32 totalShine = GameDataFunction::getTotalShineNum(*stageScene->mHolder, -1);
+	s32 unlockShine = GameDataFunction::findUnlockShineNumByWorldId(0, *stageScene->mHolder, GameDataFunction::getCurrentWorldId(*stageScene->mHolder));
 #endif
+#if SMOVER == 130
+	s32 currShine = GameDataFunction::getCurrentShineNum(stageScene->mHolder);
+	s32 totalShine = GameDataFunction::getTotalShineNum(stageScene->mHolder, -1);
+	s32 unlockShine = GameDataFunction::findUnlockShineNumByWorldId(0, stageScene->mHolder, GameDataFunction::getCurrentWorldId(stageScene->mHolder));
+#endif
+	ui.printf("Current Moons: %d\n", currShine);
+	ui.printf("Total Moons: %d\n", totalShine);
+	ui.printf("Required Moons: %d", unlockShine);
 }
