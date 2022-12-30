0777b07a61f1196ee4ae380b9675274aafc888b4
MonsterDruide1 <5958456@gmail.com>
2022-12-30 12:27:23 +0100

    Update "About" page

--- source/fl/ui/p_about.cpp
+++ source/fl/ui/p_about.cpp
@@ -3,6 +3,10 @@
 void fl::ui::about::update(PracticeUI& ui)
 {
     ui.printf("Welcome to the Practice Mod!\n");
-    ui.printf("Made by Fruityloops#8500 and contributors\n");
-    ui.printf("Big thanks to CraftyBoss, bryce_____ and Mars2030!\n");
+    ui.printf("Originally made by Fruityloops#8500\n");
+    ui.printf("TAS-Tools made by Mars2030#8008\n");
+    ui.printf("1.3-improvements by Ontwikseltsaar\n");
+    ui.printf("Further updates by MonsterDruide1#7702\n");
+    ui.printf("Big thanks to CraftyBoss and bryce_____!\n");
+    ui.printf("Special thanks to the TAS community!\n");
 }
